af00dbe159fc1441ef3af6cec2770adebbd27644
jiangbo <jiangbo@douban.com>
2017-04-03 23:33:39 +0800

    feat: exception handling

--- srt2lrc.py
+++ srt2lrc.py
@@ -5,18 +5,38 @@ python 3.x
 import glob
 
 
-def srt2lrc(num, time_str, content):
-    ts, te = time_str.replace('\n','').split(' --> ')
-    ts = ts[3:-1].replace(',','.')
-    te = te[3:-1].replace(',','.')
+def srt_block_to_irc(block):
+    infos = block.split('\n', 2)
+    if not infos or not len(infos) == 3:
+        return None
+    num, time_str, content = infos
+    ts, te = time_str.replace('\n', '').split(' --> ')
+    ts = ts[3:-1].replace(',', '.')
+    te = te[3:-1].replace(',', '.')
     co = content.replace('\n', ' ')
     return '[%s]%s\n[%s]\n' % (ts, co, te)
 
 
+def srt_file_to_irc(fname):
+    with open(fname, encoding='utf8') as file_in:
+        str_in = file_in.read()
+        blocks_in = str_in.replace('\r\n', '\n').split('\n\n')
+        blocks_out = [srt_block_to_irc(block) for block in blocks_in]
+        if not all(blocks_out):
+            err_info.append((fname, blocks_out.index(None), blocks_in[blocks_out.index(None)]))
+        blocks_out = filter(None, blocks_out)
+        str_out = ''.join(blocks_out)
+        with open(fname.replace('srt', 'lrc'), 'w', encoding='utf8') as file_out:
+            file_out.write(str_out)
+
+
 if __name__ == '__main__':
-    for fname in glob.glob('*.srt'):
-        with open(fname, encoding='utf8') as f:
-            fs = f.read()
-            out = ''.join([srt2lrc(*line.split('\n', 2)) for line in fs.replace('\r\n', '\n').split('\n\n')])
-            with open(fname.replace('srt', 'lrc'), 'w', encoding='utf8') as of:
-                of.write(out)
+    err_info = []
+    for file_name in glob.glob('*.srt'):
+        srt_file_to_irc(file_name)
+    if err_info:
+        print('success, but some exceptions are ignored:')
+        for file_name, blocks_num, context in err_info:
+            print('\tfile: %s, block num: %s, context: %s' % (file_name, blocks_num, context))
+    else:
+        print('success')
